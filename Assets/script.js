417bea9e3ab3704a0372c1eedbfe5c7de90ce900
Jbassard <jonathanacciarito@gmail.com>
2023-10-30 20:22:55 -0400

    one last last change

--- Assets/script.js
+++ Assets/script.js
@@ -79,7 +79,7 @@ $(document).ready(function () {
     $("#saved-city-list").text("");
 
     for (i = 0; i < savedCitiesArray.length; i++) {
-      var cityToDisplay = savedCitiesArray[i].cityName;
+      const cityToDisplay = savedCitiesArray[i].cityName;
       var savedCityButton = $("<button>", {
         id: "saved-city-button",
         class: "btn btn-primary mb-1",
